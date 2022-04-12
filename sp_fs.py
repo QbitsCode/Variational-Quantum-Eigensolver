3d2051ce8121af1caa3b8ea8aec8471a0826ddf6
CTLila <lila.cadi_tazi@ens-paris-saclay.fr>
2022-04-12 14:08:14 +0100

    test commit

--- sp_fs.py
+++ sp_fs.py
@@ -11,12 +11,12 @@ custom_basis = build_basis({
 }, 'Li-1,2s-ccpvdz & H-1s-sto3g')
 
 # H2 = build_molecule([('H', [0, 0, 0]), ('H', [0, 0, 1.5])], custom_basis, 0, 1, 'H2')
-He = build_molecule([('He', [0, 0, 0])], '6-31g', 0, 1, 'He')
+# He = build_molecule([('He', [0, 0, 0])], '6-31g', 0, 1, 'He')
 # H2O = build_molecule([('O', [-3.56626, 1.77639, 0]),('H', [-2.59626, 1.77639, 0.00000]),('H', [-3.88959, 1.36040, -0.81444])], 'sto3g', 0, 1,'H2O')
 # N2 = build_molecule([('N',[0,0,0]),('N',[0,0,1.0975])], 'sto-3g', 0, 1,'N2')
-# LiH = build_molecule([('Li', [0, 0, 0]), ('H', [0, 0, 0.7])], custom_basis, 0, 1, 'LiH')
+LiH = build_molecule([('Li', [0, 0, 0]), ('H', [0, 0, 0.7])], custom_basis, 0, 1, 'LiH')
 
-mymlc = He
+mymlc = LiH
 algo = VQE_fs
 
 myvqeFS = algo(
